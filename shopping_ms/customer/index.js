169569842b9eae8e83b7cf7842aa641edf88c138
gootibi <gootibi@gmail.com>
2024-08-31 02:08:18 +0200

    Add Gateway in services

--- shopping_ms/customer/index.js
+++ shopping_ms/customer/index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.use('/', (req, res, next) => {
     return res.status(200).json({ "masg": "Hello from Customer" });
--- shopping_ms/products/index.js
+++ shopping_ms/products/index.js
@@ -3,12 +3,11 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.use('/', (req, res, next) => {
     return res.status(200).json({ "masg": "Hello from Products" });
 });
 
 app.listen(8002, () => {
-    console.log(`Customer is listening on Port 8002`)
+    console.log(`Products is listening on Port 8002`)
 });
